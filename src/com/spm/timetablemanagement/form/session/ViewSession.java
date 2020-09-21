a57cb7c1fa68fec80aa29724fe003d36a5413fc6
JayawardanaJTH <tiran2323@gmail.com>
2020-09-21 13:36:42 +0530

    add session view

--- src/com/spm/timetablemanagement/form/session/ViewSession.java
+++ src/com/spm/timetablemanagement/form/session/ViewSession.java
@@ -5,19 +5,139 @@
  */
 package com.spm.timetablemanagement.form.session;
 
+import com.spm.timetablemanagement.models.Lecturer;
+import com.spm.timetablemanagement.models.Session;
+import com.spm.timetablemanagement.util.Constant;
+import com.spm.timetablemanagement.util.CreateQuery;
+import com.spm.timetablemanagement.util.DBconnection;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author Tiran Harsha
  */
 public class ViewSession extends javax.swing.JPanel {
-
+    PreparedStatement statement,statement1;
+    Connection connection;
+    ResultSet resultSet;
+    TableModel tableModel;
+    DefaultTableModel dtm;
+    
+    Dictionary<Integer, Lecturer> lecturerList;
+    Dictionary<Integer, Session> sessionList;
+    Dictionary<Integer, Integer> session_lecList;
+    
+    String[] lecture;
+    int i = 0;
     /**
      * Creates new form ViewSession
      */
     public ViewSession() {
+        this.lecturerList = new Hashtable<>();
+        this.sessionList = new Hashtable<>();
+        this.session_lecList = new Hashtable<>();
+        
         initComponents();
+        
+        try {
+            connection = DBconnection.getConnection();
+        } catch (SQLException | ClassNotFoundException | IOException ex) {
+            Logger.getLogger(ViewSession.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(this, "Error on connection create"+ex.getMessage(), "Data error", JOptionPane.ERROR_MESSAGE);
+        }
+        loadData();
     }
 
+    private void loadData(){
+        i = 0;
+        loadLecturers();
+        
+        Session session = new Session();
+        
+        try {
+            statement = connection.prepareStatement(CreateQuery.getQuery(Constant.GET_SESSION));
+            statement1 = connection.prepareStatement(CreateQuery.getQuery(Constant.GET_SESSION_LECT));
+            
+            resultSet = statement.executeQuery();
+            
+            while(resultSet.next()){
+                session.setId(resultSet.getInt("id"));
+                session.setSub(resultSet.getString("subject"));
+                session.setSubc(resultSet.getString("subject"));
+                session.setTag(resultSet.getString("tag"));
+                session.setDuration(resultSet.getString("tag_duration"));
+                session.setGroupID(resultSet.getString("group_id"));
+                session.setSubgroupID(resultSet.getString("subgroup_id"));
+                session.setStu_count(resultSet.getInt("stu_count"));
+                
+                sessionList.put(i, session);
+            }
+            resultSet.close();
+            
+            resultSet = statement1.executeQuery();
+            
+            while(resultSet.next()){
+                session_lecList.put(resultSet.getInt("session_id"),resultSet.getInt("lec_id"));
+            }
+            
+            
+            
+        } catch (SQLException | ParserConfigurationException | SAXException | IOException ex) {
+            Logger.getLogger(ViewSession.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(this, "Error on data Load"+ex.getMessage(), "Data error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    private void loadLecturers(){
+        try {
+            statement = connection.prepareStatement(CreateQuery.getQuery(Constant.GET_LECTURER));
+            resultSet = statement.executeQuery();
+            
+            while(resultSet.next()){
+                Lecturer l = new Lecturer();
+                
+                l.setId(resultSet.getInt("id"));
+                l.setName(resultSet.getString("name"));
+                l.setLec_id(resultSet.getString("emp_id"));
+                
+                lecturerList.put(l.getId(), l);
+            }
+            
+//            if(lecturerList.size()>0){
+//                lecture = new String[lecturerList.size()+1];
+//                lecture[0] = "Select";
+//            }
+//            else{
+//                lecture = new String[1];
+//                lecture[0] = "No Items";
+//            }
+//            
+//            for(i =1;i <= lecturerList.size();i++){
+//                
+//                lecture[i] = lecturerList.get(i).getName();
+//            }
+            statement.close();
+            resultSet.close();
+        } catch (ParserConfigurationException | SAXException | IOException | SQLException ex) {
+            Logger.getLogger(AddSession.class.getName()).log(Level.SEVERE, null, ex);
+            
+            JOptionPane.showMessageDialog(this, "Error on load lecturers data", "Data load error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,10 +147,15 @@ public class ViewSession extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jPanel1 = new javax.swing.JPanel();
         jPanel4 = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jPanel3 = new javax.swing.JPanel();
+
+        setLayout(new java.awt.BorderLayout());
 
         jPanel4.setBackground(new java.awt.Color(90, 30, 172));
 
@@ -44,7 +169,7 @@ public class ViewSession extends javax.swing.JPanel {
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addGap(312, 312, 312)
-                .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, 203, Short.MAX_VALUE)
+                .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, 205, Short.MAX_VALUE)
                 .addGap(337, 337, 337))
         );
         jPanel4Layout.setVerticalGroup(
@@ -55,38 +180,86 @@ public class ViewSession extends javax.swing.JPanel {
                 .addGap(20, 20, 20))
         );
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        add(jPanel4, java.awt.BorderLayout.PAGE_START);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "ID", "Lecturer(s)", "Subject", "Tag", "Duration", "GroupID", "SubgroupID", "Student Count"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane3.setViewportView(jTable1);
+        if (jTable1.getColumnModel().getColumnCount() > 0) {
+            jTable1.getColumnModel().getColumn(0).setResizable(false);
+            jTable1.getColumnModel().getColumn(1).setResizable(false);
+            jTable1.getColumnModel().getColumn(2).setResizable(false);
+            jTable1.getColumnModel().getColumn(3).setResizable(false);
+            jTable1.getColumnModel().getColumn(4).setResizable(false);
+            jTable1.getColumnModel().getColumn(5).setResizable(false);
+            jTable1.getColumnModel().getColumn(6).setResizable(false);
+            jTable1.getColumnModel().getColumn(7).setResizable(false);
+        }
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 489, Short.MAX_VALUE))
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 57, Short.MAX_VALUE)
         );
 
-        jScrollPane1.setViewportView(jPanel1);
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 842, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(114, Short.MAX_VALUE))
+        );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1)
-        );
+        jScrollPane2.setViewportView(jPanel2);
+
+        add(jScrollPane2, java.awt.BorderLayout.CENTER);
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
